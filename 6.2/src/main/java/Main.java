c49fd257da9d6920dba8cdaa62ef217ac3c38636
Anna Striganova <snowswim@mail.ru>
2016-08-29 04:33:39 +0400

    Add files via upload

---
+++ 6.2/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Аня on 29.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PropGetter.getProp("questions_en_UK"));
+    }
+}
