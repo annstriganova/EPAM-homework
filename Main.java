8d115520a0b891112be4936cef94678d217b53bb
Anna Striganova <snowswim@mail.ru>
2016-08-29 03:31:58 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Аня on 29.08.2016.
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(PropGetter.getProp("questions_en_UK"));
-    }
-}
